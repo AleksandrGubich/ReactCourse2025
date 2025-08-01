80e730b288e36c8ea22060fe1a6780080783595c
ALEKSANDR GUBICH <a.gubich@bk.ru>
2025-08-01 21:33:25 +0300

    Создание проекта + самый базовый синтаксис

---
+++ myapp/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// const text = "Hello world";
+
+// const elem = (
+//   <div>
+//     <h2 className="text">Text: {text}</h2>
+//     <label htmlFor=""></label>
+//     <input type="text" />
+//     <button tabIndex={0}>Click</button>
+//   </div>
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // elem
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
