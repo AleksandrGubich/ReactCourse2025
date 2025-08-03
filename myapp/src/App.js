6f217537526e897ae7dbc39bfbfc182617fd2b3c
ALEKSANDR GUBICH <a.gubich@bk.ru>
2025-08-03 17:52:45 +0300

    Элементы и компоненты

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,50 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
+
+export const Header = () => {
+  return <h2>Hello world</h2>;
+};
+
+// export const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: "300px",
+//   };
+//   return <input placeholder={holder} type="text" style={styledField} />;
+// };
+
+export class FieldClass extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+      width: "300px",
+    };
+
+    return <input placeholder={holder} type="text" style={styledField} />;
+  }
+}
+
+export function Btn() {
+  const text = "Log in";
+
+  // const res = () => {
+  //   return "Log in";
+  // };
+
+  // const p = <p>Log in</p>;
+
+  const logged = true;
+
+  return <button>{logged ? "Enter" : text}</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      {/* <Fields /> */}
+      <FieldClass />
+      <Btn />
     </div>
   );
 }
