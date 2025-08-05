b3b12a55ee61b55c72a21d88e4a930d6bce72a7a
ALEKSANDR GUBICH <a.gubich@bk.ru>
2025-08-05 21:45:53 +0300

    Состояния

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,16 +5,46 @@ export const Header = () => {
   return <h2>Hello world</h2>;
 };
 
-const WhoAmI = ({ name, surname, link }) => {
-  return (
-    <div>
-      <h1>
-        My name is {name.firstName}, surname - {surname}
-      </h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
-};
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: "+++",
+    };
+  }
+
+  nextYear = () => {
+    // this.setState({ years: this.state.years + 1 });
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {this.state.years}
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
+}
+
+// const WhoAmI = ({ name, surname, link }) => {
+//   return (
+// <div>
+//   <h1>
+//     My name is {name.firstName}, surname - {surname}
+//   </h1>
+//   <a href={link}>My profile</a>
+// </div>
+//   );
+// };
 
 // export const Field = () => {
 //   const holder = "Enter here";
@@ -56,11 +86,7 @@ function App() {
       {/* <Fields /> */}
       <FieldClass />
       <Btn />
-      <WhoAmI
-        name={{ firstName: "John" }}
-        surname="Smith"
-        link="facebook.com"
-      />
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
       <WhoAmI name="Alex" surname="Shepard" link="vk.com" />
     </div>
   );
