514506510bd4d1b3e3c1f57933c60d3e8b630602
ALEKSANDR GUBICH <a.gubich@bk.ru>
2025-08-11 20:45:21 +0300

    Чистый проект, без комментариев кода

--- employeeapp/employeeapp/src/components/employees-list/employees-list.js
+++ employeeapp/employeeapp/src/components/employees-list/employees-list.js
@@ -1,22 +1,24 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-import "./employees-list.css";
+import './employees-list.css';
 
-const EmployeesList = ({ data, onDelete }) => {
-  const elements = data.map((item) => {
-    const { id, ...itemprops } = item;
+const EmployeesList = ({data, onDelete}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeesListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}/>
+        )
+    })
 
     return (
-      <EmployeesListItem
-        // name={elem.name} salary={elem.salary}
-        {...itemprops}
-        key={id}
-        onDelete={() => onDelete(id)}
-      />
-    );
-  });
-
-  return <ul className="app-list list-group">{elements}</ul>;
-};
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
 
 export default EmployeesList;
